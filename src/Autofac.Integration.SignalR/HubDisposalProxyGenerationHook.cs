84ccb101692eac7ff86264935e22aed2db712199
Philip Garrett <philip.garrett@icainformatics.com>
2015-03-29 11:46:34 -0400

    Add lifetime scope support for SignalR Hubs
    Added a RegisterHubWithLifetimeScope<T> method that creates a
    dynamic proxy wrapper around T. The proxy class is then registered as
    the T component. When a T component is resolved, it creates a new
    implicit lifetime scope that is destroyed via interceptor when SignalR
    disposes the Hub. This causes all the lifetime-scoped IDisposable
    dependencies of the Hub to be cleaned up at the same time.

---
+++ src/Autofac.Integration.SignalR/HubDisposalProxyGenerationHook.cs
@@ -0,0 +1,85 @@
+﻿// This software is part of the Autofac IoC container
+// Copyright © 2013 Autofac Contributors
+// http://autofac.org
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Linq;
+using System.Reflection;
+
+using Castle.DynamicProxy;
+
+namespace Autofac.Integration.SignalR
+{
+    internal class HubDisposalProxyGenerationHook : IProxyGenerationHook
+    {
+        /// <summary>
+        /// Enables the proxy class created in <see cref="RegistrationExtensions.RegisterHubWithLifetimeScope{T}"/>
+        /// to intercept <b>only</b> the virtual Dispose(bool) method.
+        /// </summary>
+        // ReSharper disable once EmptyConstructor
+        public HubDisposalProxyGenerationHook() {}
+
+        public void MethodsInspected() {}
+
+        public void NonProxyableMemberNotification(Type type, MemberInfo memberInfo) {}
+
+        public bool ShouldInterceptMethod(Type type, MethodInfo methodInfo)
+        {
+            if (methodInfo == null)
+            {
+                throw new ArgumentNullException("methodInfo");
+            }
+
+            // only intercept "protected void Dispose(bool)"
+            return methodInfo.IsFamily
+                   && methodInfo.ReturnType == typeof (void)
+                   && methodInfo.Name == "Dispose"
+                   && methodInfo.GetParameters().Select(p => p.ParameterType).SequenceEqual(new[] {typeof (bool)});
+        }
+
+        /// <summary>
+        /// Returns true if the other object is of the same class.
+        /// This allows Castle DynamicProxy to reuse the same generated class.
+        /// </summary>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            // This class contains no data so every instance is equivalent to every other instance.
+            return obj.GetType() == GetType();
+        }
+
+        /// <summary>
+        /// Returns the same value for every instance of this class.
+        /// Implemented for good measure to match the behavior of <see cref="Equals"/>
+        /// </summary>
+        public override int GetHashCode()
+        {
+            // This class contains no data so every instance is equivalent to every other instance.
+            return GetType().GetHashCode();
+        }
+    }
+}
--- tests/Autofac.Tests.Integration.SignalR/RegistrationExtensionsFixture.cs
+++ tests/Autofac.Tests.Integration.SignalR/RegistrationExtensionsFixture.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using Autofac.Core;
 using Autofac.Integration.SignalR;
 using Microsoft.AspNet.SignalR;
@@ -34,9 +35,90 @@ namespace Autofac.Tests.Integration.SignalR
 
             Assert.That(registration.Ownership, Is.EqualTo(InstanceOwnership.ExternallyOwned));
         }
+
+        [Test]
+        public void LifetimeScopedHubIsExternallyOwned()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterHubWithLifetimeScope<TestHub>();
+            var container = builder.Build();
+
+            var service = new TypedService(typeof(TestHub));
+            IComponentRegistration registration;
+            container.ComponentRegistry.TryGetRegistration(service, out registration);
+
+            Assert.That(registration.Ownership, Is.EqualTo(InstanceOwnership.ExternallyOwned));
+        }
+
+        [Test]
+        public void LifetimeScopedHubCreatesImplicitScope()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterHubWithLifetimeScope<LifetimeScopedHub>();
+            builder.RegisterType<DisposableComponent>().InstancePerLifetimeScope();
+            using (var container = builder.Build())
+            {
+                var outerDisposable = container.Resolve<DisposableComponent>();
+                DisposableComponent innerComponent;
+                using (var hub = container.Resolve<LifetimeScopedHub>())
+                {
+                    innerComponent = hub.InnerDisposable;
+                    Assert.That(innerComponent, Is.Not.SameAs(outerDisposable),
+                        "implicit lifetime scope should require a new DisposableComponent");
+                    Assert.That(innerComponent.IsDisposed, Is.False,
+                        "the implicit scope has not been disposed yet, so its children should not be either");
+                }
+                Assert.That(innerComponent.IsDisposed, Is.True,
+                    "the implicit scope has been disposed, which should cascade to the children");
+                Assert.That(outerDisposable.IsDisposed, Is.False,
+                    "the original scope has not been disposed, so its children should not be either");
+            }
+        }
+
+        [Test]
+        public void LifetimeScopedHubCachesClassDefinition()
+        {
+            Type t1, t2;
+
+            var builder = new ContainerBuilder();
+            builder.RegisterHubWithLifetimeScope<TestHub>();
+            using (var container = builder.Build())
+            {
+                t1 = container.Resolve<TestHub>().GetType();
+            }
+
+            builder = new ContainerBuilder();
+            builder.RegisterHubWithLifetimeScope<TestHub>();
+            using (var container = builder.Build())
+            {
+                t2 = container.Resolve<TestHub>().GetType();
+            }
+
+            Assert.That(t1, Is.SameAs(t2),
+                "multiple registrations for the same hub class should reuse the same proxy class");
+        }
     }
 
     public class TestHub : Hub
     {
+    }
+
+    public sealed class DisposableComponent : IDisposable
+    {
+        public bool IsDisposed { get; set; }
+        public void Dispose()
+        {
+            IsDisposed = true;
+        }
+    }
+
+    public class LifetimeScopedHub : Hub
+    {
+        public DisposableComponent InnerDisposable { get; private set; }
+
+        public LifetimeScopedHub(DisposableComponent innerDisposable)
+        {
+            InnerDisposable = innerDisposable;
+        }
     }
 }
